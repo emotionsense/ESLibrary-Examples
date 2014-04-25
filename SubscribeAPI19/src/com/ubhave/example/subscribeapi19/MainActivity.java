2f8850dc6122f484d516c44911ab53eaf2be8fc9
Neal Lathia <neal.lathia@cl.cam.ac.uk>
2014-04-25 17:02:01 +0100

    adding example app to test subscriptions on API 19

---
+++ SubscribeAPI19/src/com/ubhave/example/subscribeapi19/MainActivity.java
@@ -0,0 +1,99 @@
+package com.ubhave.example.subscribeapi19;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.ubhave.dataformatter.DataFormatter;
+import com.ubhave.dataformatter.json.JSONFormatter;
+import com.ubhave.sensormanager.ESException;
+import com.ubhave.sensormanager.ESSensorManager;
+import com.ubhave.sensormanager.SensorDataListener;
+import com.ubhave.sensormanager.config.sensors.pull.LocationConfig;
+import com.ubhave.sensormanager.config.sensors.pull.PullSensorConfig;
+import com.ubhave.sensormanager.data.SensorData;
+import com.ubhave.sensormanager.sensors.SensorUtils;
+
+public class MainActivity extends Activity implements SensorDataListener
+{
+	private final static String LOG_TAG = "MainActivity";
+	private ESSensorManager sensorManager;
+	private JSONFormatter dataFormatter;
+	private int subscriptionId;
+	private boolean isSensing;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+//		BatterySensor battery = new BatterySensor(this);
+//		battery.sense();
+		
+		try
+		{
+			sensorManager = ESSensorManager.getSensorManager(this);
+			sensorManager.setSensorConfig(SensorUtils.SENSOR_TYPE_LOCATION, LocationConfig.ACCURACY_TYPE, LocationConfig.LOCATION_ACCURACY_FINE);
+			sensorManager.setSensorConfig(SensorUtils.SENSOR_TYPE_LOCATION, PullSensorConfig.SENSE_WINDOW_LENGTH_MILLIS, 10000L);
+			sensorManager.setSensorConfig(SensorUtils.SENSOR_TYPE_LOCATION, PullSensorConfig.POST_SENSE_SLEEP_LENGTH_MILLIS, 3000L);
+			dataFormatter = DataFormatter.getJSONFormatter(this, SensorUtils.SENSOR_TYPE_LOCATION);
+			isSensing = false;
+		}
+		catch (ESException e)
+		{
+			Log.d(LOG_TAG, e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	protected void onResume()
+	{
+		super.onResume();
+		if (!isSensing && sensorManager != null)
+		{
+			try
+			{
+				subscriptionId = sensorManager.subscribeToSensorData(SensorUtils.SENSOR_TYPE_LOCATION, this);
+				isSensing = true;
+			}
+			catch (ESException e)
+			{
+				Log.d(LOG_TAG, e.getMessage());
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Override
+	protected void onPause()
+	{
+		super.onPause();
+		if (isSensing && sensorManager != null)
+		{
+			try
+			{
+				sensorManager.unsubscribeFromSensorData(subscriptionId);
+				isSensing = false;
+			}
+			catch (ESException e)
+			{
+				Log.d(LOG_TAG, e.getMessage());
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Override
+	public void onDataSensed(SensorData data)
+	{
+		Log.d(LOG_TAG, dataFormatter.toJSON(data).toString());
+	}
+
+	@Override
+	public void onCrossingLowBatteryThreshold(boolean isBelowThreshold)
+	{
+		Log.d(LOG_TAG, "onCrossingLowBatteryThreshold: "+isBelowThreshold);
+	}
+}
